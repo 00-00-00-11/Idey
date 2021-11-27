b1c6aa911f80f681f9c9af2a3c69e7e317743a13
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2021-11-27 19:38:17 +0100

    Modelist added, but doesnt work (mauro pls help)

--- src/static/js/ide_file.js
+++ src/static/js/ide_file.js
@@ -7,4 +7,11 @@ ipcRenderer.on("file_path", (event, arg) => {
     var filePath = arg;
 
     document.title = `${path.basename(filePath)} | Idey`;
+
+    var editor = ace.edit("ide");
+    var modelist = ace.require("ace/ext/modelist");
+    var mode = modelist.getModeForPath(filePath).mode;
+    editor.session.setMode(mode);
+
+    editor.setValue(fs.readFileSync(filePath, "utf8"));
 });
