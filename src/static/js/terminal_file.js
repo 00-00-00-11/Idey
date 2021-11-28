bf3ed9a99dc1f47ae18cd6b66b3f60788f6c2fcc
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2021-11-28 12:07:15 +0100

    change a few small things

--- src/static/js/terminal_file.js
+++ src/static/js/terminal_file.js
@@ -4,11 +4,11 @@ const { Terminal } = require("xterm")
 document.getElementById("terminal_btn").onclick = () => {
     document.querySelector(".file_ide_grid").classList.add("file_ide_grid_terminal");
     document.querySelector("#terminal").classList.remove("display_none");
+    document.querySelector(".main_menu_container").classList.add("display_none");
 
     var term = new Terminal();
     term.open(document.getElementById('terminal'));
-    term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+    term.write('TODO: Terminal')
 
-    term.resize()
-
+    editor.resize();
 }
