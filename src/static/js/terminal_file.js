b6a3842d9fb7af1371d3d119dab39ef675ae513e
mauro-balades <mauro.balades@tutanota.com>
2021-11-28 09:39:35 +0100

    added a terminal

--- src/static/js/terminal_file.js
+++ src/static/js/terminal_file.js
@@ -1,7 +1,14 @@
+const { Terminal } = require("xterm")
+
+
 document.getElementById("terminal_btn").onclick = () => {
     document.querySelector(".file_ide_grid").classList.add("file_ide_grid_terminal");
     document.querySelector("#terminal").classList.remove("display_none");
 
-    //hey mauro do whatever you want
-    //maybe use xterm?
+    var term = new Terminal();
+    term.open(document.getElementById('terminal'));
+    term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+
+    term.resize()
+
 }
